da37fd32ab817720ebb6a636412522eaf19be670
USAMI Kosuke <usami.kosuke@gmail.com>
2022-03-24 16:00:01 +0900

    Add SwiftLint plugin

--- Hello/AppFeature/Package.swift
+++ Hello/AppFeature/Package.swift
@@ -15,11 +15,23 @@ let package = Package(
         .package(path: "../Core"),
     ],
     targets: [
+        // https://github.com/juozasvalancius/ExampleSPMProjectWithSwiftLint
+        .binaryTarget(
+            name: "SwiftLintBinary",
+            url: "https://github.com/juozasvalancius/SwiftLint/releases/download/spm-accommodation/SwiftLintBinary-macos.artifactbundle.zip",
+            checksum: "cdc36c26225fba80efc3ac2e67c2e3c3f54937145869ea5dbcaa234e57fc3724"
+        ),
+        .plugin(
+            name: "SwiftLintXcode",
+            capability: .buildTool(),
+            dependencies: ["SwiftLintBinary"]
+        ),
         .target(
             name: "AppFeature",
             dependencies: [
                 "Core"
-            ]),
+            ],
+            plugins: ["SwiftLintXcode"]),
         .testTarget(
             name: "AppFeatureTests",
             dependencies: ["AppFeature"]),
--- README.md
+++ README.md
@@ -7,3 +7,9 @@ Xcode project with SwiftPM
 - アプリの実装コードは、Swiftパッケージ（`AppFeature`、`Core`）に入れる。
 - `Hello.xcodeproj` でアプリに `AppFeature` パッケージをリンクする。
 
+## SwiftPMプラグイン
+
+- Xcodeプロジェクトでビルドスクリプトを設定する代わりに、SwiftPMのビルドツールプラグインを利用する。
+- Xcode上でビルドを実行すると、SwiftLintが実行されてXcode上でwarningが表示される。
+- Thanks to https://github.com/juozasvalancius/ExampleSPMProjectWithSwiftLint
+
